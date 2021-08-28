efc576830824df8fcce3c1f498347177b42f64ff
Kostya <kovevare@gmail.com>
2021-08-28 22:49:29 +0300

    full album return

--- functions/3-create-new-song.js
+++ functions/3-create-new-song.js
@@ -1,25 +1,36 @@
 //dotenvpackage for safe apikey
 require('dotenv').config()
-
 var Airtable = require('airtable');
-        var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY})
-        .base('appDh21gcCzW2v02j');
 
-        base('songs').create({
-        "name": "divide",
-        "desc": "divide",
-        "image": [
-            {
-            "url": "https://images-na.ssl-images-amazon.com/images/I/61MHPCXEdFS._SL1369_.jpg"
-            }
-        ],
-        "price": 60,
-        "artist": "ed sheeran",
-        "album": "divide"
-        }, function(err, record) {
-        if (err) {
-            console.error(err);
-            return;
+var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY})
+.base('appDh21gcCzW2v02j');
+
+exports.handler = async (event, context) =>{
+    const {name, desc, url, price, artist, album} = event.queryStringParameters;
+
+    base('songs').create({
+    "name": name,
+    "desc": desc,
+    "image": [
+        {
+        "url": url
         }
-        console.log(record.getId());
+    ],
+    "price": price,
+    "artist": artist,
+    "album": album
+
+    }, function(err, record) {
+    if (err) {
+        return {
+            statusCode: 500,
+            body: 'general server error',
+        }
+    }
+    console.log(record.getId());
 });
+return {
+        statusCode: 200,
+        body: 'created successfully'
+        }
+}
